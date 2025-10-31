475e12d3b934bd1a0b0f7e0adb7df0d61f271f23
Khairi Abidi <abidikhairi.1337@gmail.com>
2025-10-31 21:32:41 +0100

    add function Personne::marier

--- src/tp3/ex2/Personne.java
+++ src/tp3/ex2/Personne.java
@@ -1,5 +1,7 @@
 package tp3.ex2;
 
+import java.util.Calendar;
+
 public class Personne {
 
     public String nom;
@@ -29,6 +31,40 @@ public class Personne {
 
             else
                 System.out.println("Mme. " + this.nom + " " + this.prenom + " est née en " + this.annnee_nais + " elle est " + this.etatc);
+
+        }
+    }
+
+    public int age() {
+        Calendar c = Calendar.getInstance();
+        int anneeCourante = c.get(Calendar.YEAR);
+
+        return anneeCourante - annnee_nais;
+    }
+
+    public void marier(Personne p1) {
+        if ((this.age() >= 18) && (p1.age() >= 18)) {
+            if (this.sexe != p1.sexe) {
+                if ((this.conjoint == false) && (p1.conjoint == false) && (p1.sexe == 'H')) {
+                    p1.etatc = "marié";
+                    p1.conjoint = true;
+                    this.etatc = "mariée";
+                    this.conjoint = true;
+                    this.nom = p1.nom + this.nom;
+                } else if ((this.conjoint == false) && (p1.conjoint == false) && (p1.sexe == 'F')) {
+                    this.etatc = "marié";
+                    this.conjoint = true;
+                    p1.etatc = "mariée";
+                    p1.conjoint = true;
+                    p1.nom = this.nom + p1.nom;
+                } else {
+                    System.out.println("Impossible de marier 2 personnes déjà mariées !");
+                }
+            } else {
+                System.out.println("Impossible de marier 2 personnes de Même sexe !");
+            }
+        } else {
+            System.out.println("Impossible de marier 2 personnes qui ne sont pas majeurs !");
 
         }
     }
--- src/tp3/ex2/TestPersonne.java
+++ src/tp3/ex2/TestPersonne.java
@@ -3,10 +3,14 @@ package tp3.ex2;
 public class TestPersonne {
 
     public static void main(String[] args) {
-        Personne P1 = new Personne("John ","Doe",'H',2000,"célibataire",false);
-        Personne P2 = new Personne("Jane","Doe",'F',2002,"célibataire",false);
+        Personne P1 = new Personne("Abidi ","Ali",'H',2000,"célibataire",false);
+        Personne P2 = new Personne("Wilani","Wassila",'F',2002,"célibataire",false);
 
         System.out.println("******* Informations avant le mariage **********");
+        P1.retourneInfos();
+        P2.retourneInfos();
+        P1.marier(P2);
+        System.out.println("******* Informations aprés le mariage **********");
         P1.retourneInfos();
         P2.retourneInfos();
     }
